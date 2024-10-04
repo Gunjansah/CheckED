883f12e95675f4f3f6361608c5e1bd1e22578409
Gunjansah <sahgunjan04@gmail.com>
2024-10-04 11:58:48 -0500

    Add project files.

---
+++ CheckED/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CheckED
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CheckED/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace CheckED
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
